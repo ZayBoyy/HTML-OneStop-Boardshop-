2e26ed307e3e704bde84d4e331d8df3577678fbd
Zay Davis <112731206+ZayBoyy@users.noreply.github.com>
2022-12-07 13:38:47 -0700

    More Styling Changes

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navElements = document.querySelector(".mobile-nav");
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        navElements.classList.remove('hidden');
+
+        menuOpen = true;
+    }
+    else {
+        menuBtn.classList.remove('open');
+        navElements.classList.add('hidden');
+        menuOpen = false;
+    }
+});
